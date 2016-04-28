6b7990da8623afd7d8ae14a1ee8d7b319e6957ba
Mike Meyer <github.com@meyer.fm>
2016-04-28 00:06:20 -0700

    command line flags, baby

--- daemon/main.go
+++ daemon/main.go
@@ -1,17 +1,29 @@
 package main
 
 import (
+	"flag"
 	"github.com/gorilla/mux"
 	"log"
 	"net/http"
 )
 
 func main() {
-	log.Println("dexd " + DexVersion + " at your service")
-	r := mux.NewRouter()
-	r.HandleFunc("/", indexHandler)
-	r.HandleFunc("/{url:[^\\.]+\\.[^\\.]+}.{ext:json}", siteHandler)
-	r.HandleFunc("/{cachebuster:\\d+}/empty.{ext:(js|css)}", emptyHandler)
-	r.HandleFunc("/{cachebuster:\\d+}/{url:(global|[^\\.]+\\.[^\\.]+)}.{ext:(js|css)}", siteHandler)
-	http.ListenAndServeTLS(":3132", "cert.pem", "key.pem", r)
+	runPtr := flag.Bool("run", false, "start dexd "+DexVersion)
+	installPtr := flag.Bool("install", false, "install launch agent")
+	flag.Parse()
+
+	switch {
+	case *runPtr:
+		log.Println("dexd " + DexVersion + " at your service")
+		r := mux.NewRouter()
+		r.HandleFunc("/", indexHandler)
+		r.HandleFunc("/{url:[^\\.]+\\.[^\\.]+}.{ext:json}", siteHandler)
+		r.HandleFunc("/{cachebuster:\\d+}/empty.{ext:(js|css)}", emptyHandler)
+		r.HandleFunc("/{cachebuster:\\d+}/{url:(global|[^\\.]+\\.[^\\.]+)}.{ext:(js|css)}", siteHandler)
+		http.ListenAndServeTLS(":3131", "cert.pem", "key.pem", r)
+	case *installPtr:
+		log.Println("Install!")
+	default:
+		flag.PrintDefaults()
+	}
 }
